528f866b670d958b8056a123aadd64842f34c54c
salvadorsalazar <salvador.g.salazar79@gmail.com>
2023-07-13 01:01:03 -0500

    2nd

--- app/public/experiment.php
+++ app/public/experiment.php
@@ -1,5 +1,7 @@
-<?php echo 2+2 ?>
+<?php echo 2+2  ; echo $name = "sal" ; ?>
 
 
-<h1>Hello Wordpress</h1>
-<h2>this is about you</h2>
+<h1>Hello Wordpress , your name is <?php echo $name ?></h1>
+<?php echo 5*22 ?>
+
+<h2>this is about you , and you are <?php echo $name ?></h2>
