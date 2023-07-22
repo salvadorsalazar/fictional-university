e37ed03c7d306ad3f159fb916120f8fe32433f8a
salvadorsalazar <salvador.g.salazar79@gmail.com>
2023-07-22 03:11:29 -0500

    update

--- app/public/experiment.php
+++ app/public/experiment.php
@@ -5,3 +5,41 @@
 <?php echo 5*22 ?>
 
 <h2>this is about you , and you are <?php echo $name ?></h2>
+
+
+
+
+<!-- 
+
+
+
+
+
+<h1>THIS IS OUR CUSTOM THEME</h1>
+
+<?php
+// function myFrirst() {
+
+//     echo "<p>first function </p>";
+// }
+
+
+function greet($name,$color){
+    echo "<p> Hello my name is $name and my favorite color is $color</p>";
+
+}
+// myFrirst();
+greet('john','blue');
+greet('jane','green');
+
+?>
+<!-- 
+<p>Hello my name is sal and my favorite color is blue</p>
+<p>Hello my name is sal and my favorite color is blue</p> -->
+
+<h1><?php bloginfo('name'); ?></h1>
+<p><?php bloginfo('description'); ?></p>
+
+
+
+ -->
--- app/public/wp-content/themes/fictional theme/index.php
+++ app/public/wp-content/themes/fictional theme/index.php
@@ -1,24 +1,9 @@
-<h1>THIS IS OUR CUSTOM THEME</h1>
+<?php 
 
-<?php
-// function myFrirst() {
-
-//     echo "<p>first function </p>";
-// }
+$names = array('sal','aden','scxarlett');
 
 
-function greet($name,$color){
-    echo "<p> Hello my name is $name and my favorite color is $color</p>";
-
-}
-// myFrirst();
-greet('john','blue');
-greet('jane','green');
 
 ?>
-<!-- 
-<p>Hello my name is sal and my favorite color is blue</p>
-<p>Hello my name is sal and my favorite color is blue</p> -->
 
-<h1><?php bloginfo('name'); ?></h1>
-<p><?php bloginfo('description'); ?></p>
+<p>hello my name is <?php echo $names[1]; ?></p>
