81b65c343886879426e8dfd04d73c3de18a500cd
salvadorsalazar <salvador.g.salazar79@gmail.com>
2023-07-23 14:48:53 -0500

    arrays

--- app/public/wp-content/themes/fictional theme/index.php
+++ app/public/wp-content/themes/fictional theme/index.php
@@ -2,8 +2,15 @@
 
 $names = array('sal','aden','scxarlett');
 
+$count=0;
 
+while($count <100){
+    echo "<br>$count</br>";
+    
+    $count++;
+}
 
 ?>
 
 <p>hello my name is <?php echo $names[1]; ?></p>
+
